6532c4c69a258a7f6c88f9862126b388b06230b5
SkullKrak7 <68808429+SkullKrak7@users.noreply.github.com>
2026-02-04 01:59:56 +0000

    style: format docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,35 +2,36 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
-project = 'RAG Demo'
-copyright = '2026, Sai Karthik Kagolanu'
-author = 'Sai Karthik Kagolanu'
-release = '1.0.0'
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "RAG Demo"
+copyright = "2026, Sai Karthik Kagolanu"
+author = "Sai Karthik Kagolanu"
+release = "1.0.0"
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-    'myst_parser',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "myst_parser",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    'undoc-members': True,
-    'exclude-members': '__weakref__'
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
 }
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
+    "python": ("https://docs.python.org/3", None),
 }
